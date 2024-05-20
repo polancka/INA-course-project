839bf2e52011f7c1bdfa504e943c95ac04a9e394
polancka <ana.polancec@gmail.com>
2024-05-20 13:14:27 +0200

    Corrected graph, test jsons for just one graphlet

--- graphlet_finder.py
+++ graphlet_finder.py
@@ -27,6 +27,26 @@ def draw_histogram(graphlet_counts):
     plt.tight_layout()
     plt.show()
 
+def draw_line_graph(data):
+    # Extract unique x-labels (assuming all lines have the same x-labels for simplicity)
+    x_labels = list(data['classic_multi_graph.net'].keys())
+
+    # Creating the line graph for each entry in the dictionary
+    for line_label, values_dict in data.items():
+        y_values = [values_dict[label] for label in x_labels]
+        plt.plot(x_labels, y_values, marker='o', label=line_label)
+
+    # Adding title and labels
+    plt.title('Graphlets based on genre')
+    plt.xlabel('Graphlets')
+    plt.ylabel('Genre graphs')
+
+    # Adding a legend
+    plt.legend()
+
+    # Display the graph
+    plt.show()
+
 def count_graphlet_occurrences(adj_list, graphlet):
     count = 0
     graphlet_size = len(graphlet.nodes)
@@ -55,16 +75,17 @@ def main():
 
     directory_path = 'graphs/'
     json_save_directory = 'graphlet_jsons/'
-
+    end_graphlets = {}
     for filename in os.listdir(directory_path):
         file_path = os.path.join(directory_path, filename)
         print(file_path)
 
         print("counting graphlets") 
-        graph = nx.read_pajek("graphs/notes_graph.net")
+        graph = nx.read_pajek(file_path)
         adj_list = graph_to_adjacency_list(graph)
         atlas = nx.graph_atlas_g()
-        graphlets = [G for G in atlas if 2 == len(G) and nx.is_connected(G)]
+        graphlets = [G for G in atlas if 2 <= len(G) <= 4 and nx.is_connected(G)]
+
         graphlet_counts = {}
         list_nodes = graph.nodes
         for graphlet in graphlets:
@@ -76,8 +97,8 @@ def main():
         for graphlet, count in graphlet_counts.items():
             print(f"Graphlet:\n{graphlet}\nCount: {count}\n")
         
-        draw_histogram(graphlet_counts) #TODO: change from histogram to line graph
-
+        #draw_histogram(graphlet_counts) #TODO: change from histogram to line graph
+        end_graphlets[filename] = graphlet_counts # save for later visualisation
         # Convert and write JSON object to file
         json_filename = f"{filename}.json"
         json_file_path = os.path.join(json_save_directory, json_filename)
@@ -85,6 +106,7 @@ def main():
             json.dump(graphlet_counts, json_file, indent=4)
 
         #TODO: line graph for all netwroks at once
+    draw_line_graph(end_graphlets)
 
 # if __name__ == "__main__":
 #     main()
